fd7e5693f013080f1a28a6acee4c5c9eecff5fb7
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-09-08 17:54:05 +0900

    save

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 Cloning Youtube with VanillaJS and NodeJS
 
 9/3 : #0.0 ~ 2.10
+9/4 : #2.11 ~ 2.14
+9/5 : #2.15 ~ 2.18
+9/8 : #2.19 ~ 
+
+## Pages:
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,20 +2,23 @@
 
 import express from "express";
 import routes from "../routes";
+import {
+  users,
+  editProfile,
+  userDetail,
+  changePassword,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send("Users"));
-userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
-userRouter.get(routes.editProfile, (req, res) => res.send("Edit Profile"));
-userRouter.get(routes.changePassword, (req, res) =>
-  res.send("Change Password")
-);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
 
-// Router |  http://localhost:4000/user
-// Router |  http://localhost:4000/user/edit
+// Router |  http://localhost:4000/user/1
+// Router |  http://localhost:4000/user/edit-profile
 // Router |  http://localhost:4000/user/password
 
 /*
