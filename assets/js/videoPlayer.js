8e07b55b1180e19d0da0be9b0ca2eabdc31dc28a
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-10-06 12:53:35 +0900

    #8.2 custom video player | playBtn, muteBtn, FullscreenBtn

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumnBtn = document.getElementById("jsVolumnBtn")
+const fullScreenBtn = document.getElementById("jsFullScreen")
 
 
 function handlePlayClick() {
@@ -26,10 +27,24 @@ function handleVolumnClick() {
   }
 }
 
+function goFullScreen() {
+  videoContainer.webkitRequestFullscreen();
+  fullScreenBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScreenBtn.removeEventListener("click", goFullScreen)
+}
+
+function exitFullScreen() {
+  fullScreenBtn.innerHTML = '<i class="fas fa-expand"></i>';
+  fullScreenBtn.addEventListener("click", goFullScreen)
+  document.webkitExitFullscreen()
+}
+
 function init() {
   //해당 페이지에 있다는것을 체크하기 위함.
   playBtn.addEventListener("click", handlePlayClick);
   volumnBtn.addEventListener("click", handleVolumnClick);
+  fullScreenBtn.addEventListener("click", goFullScreen);
+  fullScreenBtn.addEventListener("click", exitFullScreen);
 }
 
 if (videoContainer) {
@@ -44,6 +59,12 @@ if (videoContainer) {
 //NOTE 방법, 방식(method)-메소드
 // HTMLMediaElement.play() 미디어를 재생합니다.
 // HTMLMediaElement.pause() 미디어 재생을 일시 정지합니다.
+
+//NOTE https://developer.mozilla.org/en-US/docs/Web/API/Element/requestFullScreen
+//NOTE https://developer.mozilla.org/en-US/docs/Web/API/Document/exitFullscreen
+//Element.requestFullscreen() & Document.exitFullscreen()
+//play나 mute처럼 if & esle가 가능한 기능은 제공되지 않기 때문에 풀스크린(requestFullscreen) 확대(expand) 기능을 만들었다가, 다시 반대로 축소해서(compress) 나가는 기능(exitFullscreen)을 만들어야 한다.
+//webkit : 크롬에서 완전한 지원이 되지 않아 추가 입력. (webkitRequestFullscreen ,webkitExitFullscreen)
 
 /* NOTE 코드 설명
 자바스크립트 파일은 모든 페이지에 로드된다 footer 밑에 include되기 때문이다.
